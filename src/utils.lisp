0e58c5b53fed9fd61c795885079cbb927b023264
madand <dev@madand.net>
2020-01-03 08:16:15 +0200

    Add mouse click simulation command

--- src/utils.lisp
+++ src/utils.lisp
@@ -3,6 +3,8 @@
 (defpackage :stumpwm.d/utils
   (:documentation "StumpWM configuration utilities.")
   (:use #:cl)
+  (:local-nicknames
+   (#:swm #:stumpwm))
   (:import-from #:stumpwm.d/variables
                 #:*terminal-cmd*)
   (:export
@@ -12,3 +14,22 @@
 (defun format-exec-in-terminal (command)
   "Build exec command with the terminal emulatior. See *TERMINAL-CMD*."
   (format nil "exec ~A -e '~A'" *terminal-cmd* command))
+
+;;; ----------------------------------------------------------------------------
+;;; Simulation of mouse (rat) clicks
+;;; ----------------------------------------------------------------------------
+
+(defun send-rat-click (button iterations)
+  "Send a mouse click to the current pointer location.
+‘button’ is which mouse button to use and ‘iterations’ is how many times to
+click (so twice would be a double-click, for example).
+
+The implamentation requires xte utility program to be present in PATH. One need
+to install xautomation(7) package, which provides xte."
+  (loop :repeat iterations
+        :do (shell-command (format nil "xte 'mouseclick ~A" button))))
+
+(swm:defcommand rat-click (button iterations)
+    ((:number "Button: ") (:number "How many times? "))
+  (when (current-window)
+    (send-rat-click button iterations)))
