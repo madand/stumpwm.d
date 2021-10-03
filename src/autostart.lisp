cff1d163e61626ef4d00d7b48b8b3334f0e63e7d
madand <dev@madand.net>
2021-10-03 17:35:36 +0300

    Restart picom after monitor connect/disconnect

--- src/autostart.lisp
+++ src/autostart.lisp
@@ -8,6 +8,22 @@
    (#:vars :stumpwm.d/variables)))
 (in-package :stumpwm.d/autostart)
 
+(defun desktop-background-command ()
+  "Return shell command that sets the desktop background."
+  (format nil "feh --bg-max ~A" vars:*bg-image*))
+
+(defun run-desktop-background-command (&rest _)
+  "Set the desktop background via a shell command.
+See the function `desktop-background-command'. The ignored arg is for the
+compatibility with `stumpwm::*new-head-hook*'."
+  (declare (ignore _))
+  (swm:run-shell-command (desktop-background-command)))
+
+(defun restart-picom (&rest _)
+  "Restart the `picom' compositor."
+  (declare (ignore _))
+  (swm:run-shell-command "pkill $(pidof picom)"))
+
 (when swm:*initializing*
   ;; Set desktop background.
   (swm:run-shell-command (format nil "feh --bg-fill ~A" vars:*bg-image*))
@@ -15,5 +31,6 @@
   (swm:run-shell-command "xsetroot -cursor_name left_ptr")
   ;; Load xmodmap(1) configuration.
   (swm:run-shell-command "xmodmap ~/.Xmodmap")
-  ;; Picom is a fork of Compton: https://github.com/yshui/picom/
-  (swm:run-shell-command "picom -b --dbus"))
+
+;; picom seems to stop working correctly after monitor hot-plug.
+(swm:add-hook swm:*new-head-hook* 'restart-picom)
